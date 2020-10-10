598a0e57525fddcd2f6b1d9824716810a48cfcd9
ramadhan ibnu <ramadhanibnu77@gmail.com>
2020-10-10 22:41:13 +0700

    initial commit

---
+++ back-end/models/Member.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const memberSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    email: String,
+    password: String,
+    pc:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"pc-room"
+    }
+});
+
+const Member = mongoose.model("member", memberSchema);
+
+module.exports = Member
